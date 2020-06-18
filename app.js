9f16cbd6b924aa2ce6e7266fcf3be6c203d04b81
damiancipolat <damian.cipolat@gmail.com>
2020-06-18 14:32:09 -0300

    add

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const {
     getForecastByCity
 } = require('./lib/weather.js');
 
-ipapi.getLocation().then(a=>console.log(a)).catch(e=>console.error(e));
-
-ipapi.getCity().then(a=>console.log(a)).catch(e=>console.error(e));
+//ipapi.getLocation().then(a=>console.log(a)).catch(e=>console.error(e));
+//ipapi.getCity().then(a=>console.log(a)).catch(e=>console.error(e));
 
 getWeatherByCity('Buenos Aires').then(a=>console.log(a)).catch(e=>console.error(e));
-getForecastByCity('Buenos Aires').then(a=>console.log(a)).catch(e=>console.error(e));
+//getForecastByCity('Buenos Aires').then(a=>console.log(a)).catch(e=>console.error(e));
 //fb24bdedbc06fec558d52fc020795211
-
